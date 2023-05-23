33fea3e7cda9933002ebe205e2c949d1eceaadf3
Luis Santos <luis.santos@lab900.com>
2023-05-23 12:19:29 +0200

    feat: firebase login

--- app.js
+++ app.js
@@ -1,7 +1,88 @@
-function somhi() {
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyA7selI9Lv-BywgQXPGhZfs-ogS5HctzkA",
+
+    authDomain: "gday-bf969.firebaseapp.com",
+
+    projectId: "gday-bf969",
+
+    storageBucket: "gday-bf969.appspot.com",
+
+    messagingSenderId: "525677684037",
+
+    appId: "1:525677684037:web:49a2b83e2dea3cd6f38a2c",
+
+    measurementId: "G-Q3XLHYFJ08"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+getAnalytics(app);
+
+const auth = getAuth(app);
+
+let user = auth.currentUser;
+
+if (location.pathname !== "/login/") {
+    if (user == null) {
+        location.assign("/login");
+    } else {
+        console.log(user)
+    }
+}
+
+
+window.loginUser = (e) => {
+    e.preventDefault();
+    var form = document.getElementById('loginForm');
+
+    console.log("form submitted");
+    var email = form.elements.email.value;
+    var password = form.elements.password.value;
+    console.log(email, password)
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            user = userCredential.user;
+            location.assign("/");
+            // ...
+        })
+        .catch((error) => {
+            console.log(error);
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(`${errorCode} - ${errorMessage}`);
+        });
+};
+
+
+
+
+window.somhi = () => {
     location.assign("/itinerary");
 }
 
-function home() {
+window.goHome = () => {
     location.assign("/");
 }
